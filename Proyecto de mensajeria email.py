ed3512c8eaeb2c78bad35984510c20bd33d0ac29
lauraromeroforero <111913832+lauraromeroforero@users.noreply.github.com>
2022-11-22 15:49:04 -0500

    Version tres

--- Proyecto de mensajeria email.py
+++ Proyecto de mensajeria email.py
@@ -5,16 +5,18 @@ import smtplib
 
 email_emisor = 'en17la@gmail.com'
 email_contrasena = 'piscdattqdcmojci'
-#prueba con mas de dos correos
-email_receptor = 'yuliysandrita123@gmail.com', 'contrekalo@gmail.com', 'en17la@gmail.com', 'forero0801laura@gmail.com', 'minnielaura27@gmail.com'
-
+#prueba con correos
+email_receptor = 'yuliysandrita123@gmail.com', 'contrekalo@gmail.com', 'en17la@gmail.com'
+#Pusimos el asunto del correo para que se vea mas organizado
+asunto = 'PRUEBA DE EMAIL CON GMAIL'
 cuerpo = """
-Si se pudo Profesor Hugo Ruiz PRUEBA 2 \n ATT:Laura Lorena Forero Romero
+Si se pudo Profesor Hugo Ruiz PRUEBA 3 \n ATT:Laura Lorena Forero Romero
 """
 
 em = EmailMessage()
 em['From'] = email_emisor
 em['To'] = email_receptor
+em['Subject'] = asunto
 em.set_content(cuerpo)
 
 contexto = ssl.create_default_context()
@@ -22,3 +24,4 @@ contexto = ssl.create_default_context()
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=contexto) as smtp:
     smtp.login(email_emisor, email_contrasena)
     smtp.sendmail(email_emisor, email_receptor, em.as_string())
+
