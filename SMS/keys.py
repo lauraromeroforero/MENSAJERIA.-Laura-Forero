9e39fcd36fd367eafb69123c9647fc05384cb6de
lauraromeroforero <111913832+lauraromeroforero@users.noreply.github.com>
2022-12-01 11:24:35 -0500

    Versión Dos

--- SMS/keys.py
+++ SMS/keys.py
@@ -1,3 +1,7 @@
 #Definimos el Id y el token entregados por twilio
 account_sid = 'AC5201afc8a15c76025a476d6b59a48971'
 auth_token = '0cafcfa09f557fdd922e0c95bb41a4a0'
+
+#Ponemos el numero que nos da twilio y el numero de nosotros
+twilio_number = '+18316035159'
+target_number = '+573043576566'
--- SMS/main.py
+++ SMS/main.py
@@ -5,3 +5,10 @@ import keys
 
 #Definimos el cliente con los datos del key
 client = Client(keys.account_sid, keys.auth_token)
+
+#Definimos el cuerpo del mensaje y el destinatario con los numeros de twilio
+message = client.messages.create(
+    body="Te amo cosita bella",
+    from_=keys.twilio_number,
+    to=keys.target_number
+)
