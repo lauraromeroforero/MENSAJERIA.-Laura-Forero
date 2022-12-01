46070e355b12347f70c156035f3aa6f517e81f26
lauraromeroforero <111913832+lauraromeroforero@users.noreply.github.com>
2022-12-01 11:07:33 -0500

    Versión Dos

--- message WhatsApp - Laura Forero.py
+++ message WhatsApp - Laura Forero.py
@@ -4,3 +4,11 @@ from twilio.rest import Client
 account_sid = 'AC5201afc8a15c76025a476d6b59a48971' 
 auth_token = '0cafcfa09f557fdd922e0c95bb41a4a0' 
 client = Client(account_sid, auth_token) 
+
+#Establezco el mensaje con el numero que me da twilio y el de nosotros 
+#registrado en twilio
+message = client.messages.create( 
+from_='whatsapp:+14155238886',  
+body='Viva millos',      
+to='whatsapp:+573043576566' 
+)
